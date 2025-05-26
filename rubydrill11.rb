7ca65721c0955269cc6842082f165d0e37e3bcfa
yuda12c <ngsrn.910@gmail.com>
2025-05-26 20:50:09 +0900

    Create rubydrill11.rb

---
+++ rubydrill11.rb
@@ -0,0 +1,15 @@
+def check_name(str) 
+  if str.include?(".")
+    puts "!エラー!記号は登録できません"
+  elsif str.include?(" ")
+    puts "!エラー!空白は登録できません"
+  else
+    puts "登録が完了しました"
+  end
+end
+
+puts "登録したい名前を入力してください(例)YamadaTaro"
+str = gets.chomp #getsだけでもOK
+check_name(str) 
+
+#正答
