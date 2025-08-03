07fba1a4d809e632df0e4839e0d6d376d27f7734
yuda12c <ngsrn.910@gmail.com>
2025-08-03 20:16:20 +0900

    33.34

---
+++ rubydrill33.rb
@@ -0,0 +1,6 @@
+def left2(str)
+  puts str[2..-1] + str[0..1]
+end
+
+# 呼び出し例
+left2("Hello")
---
+++ rubydrill34.rb
@@ -0,0 +1,7 @@
+def array_count9(nums)
+  nums_9 = nums.count(9)
+  puts "配列の中には9が#{nums_9}個です"
+end
+
+# 呼び出し例
+array_count9([1, 2, 9]) 
